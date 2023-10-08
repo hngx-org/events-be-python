c7cf0f58c5b6c9d8eb7da012f6e87416434876f7
Oluwafikayomi Soetan <soetandan@gmail.com>
2023-10-08 12:44:02 +0100

    feat: added the search endpoint

--- events/urls.py
+++ events/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('/api/events', views.EventsView.as_view()),
+    path('/api/events/<uuid:event_id>', views.getEvent.as_view(), name="get-event-by-id"),
+    path('/api/events/search/<str:keyword>', views.SearchEventView.as_view(), name="search-event")
 ]
