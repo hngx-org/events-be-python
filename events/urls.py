b52021fa50c43fe92e4231a50d67261e844bd4bd
JesseGreat <oluwatobilobaokunogbe@gmail.com>
2023-10-08 09:41:12 -0700

    endpoint to edit user groups

--- events/urls.py
+++ events/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import userGroupsSerializer
 
 
 
@@ -12,5 +13,6 @@ urlpatterns = [
     path('events/search/', views.SearchEventView.as_view(), name='event-search'),
     path('events/<int:event_id>/update/', views.update_event, name='event-update'),
     path('events/calendar', views.CalenderView.as_view(), name='calender'),
-    path('events/<int:id>',views.EventDelView.as_view(), name='Delevent')
+    path('events/<int:id>',views.EventDelView.as_view(), name='Delevent'),
+    path('edit-group/<int:pk>/', userGroupsSerializer.as_view(), name='edit-group')
 ]
