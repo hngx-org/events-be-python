8eac24f5315f200c34fa869c427510dcb2528533
uthmanduro <udurosinlohun@gmail.com>
2023-10-08 08:59:45 +0100

    implemented the get all events endpoint

--- events/views.py
+++ events/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Events
+from .serializers import EventsSerializer
+from django.http import JsonResponse
 
-# Create your views here.
+
+def get_all_events(request):
+    """
+    Provides a get method handler that returns all events.
+    """
+    queryset = Events.objects.all()
+    serializer_class = EventsSerializer
+    return JsonResponse(serializer_class.data, safe=False)
