8bb5970dee9262d4324068b5146d1a5a963af42c
Ifechukwu Ogidi <ogidiifechukwu6@gmail.com>
2023-10-08 13:43:21 +0100

    Feat: Added authentication for users

--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
@@ -8,10 +8,13 @@ from django.shortcuts import get_object_or_404
 from .models import Comment
 from .serializers import CommentSerializer
 from events.models import Events
+from users.authentication import AuthenticationMiddleware, IsAuthenticatedUser
 
 
 # Create your views here.
 @api_view(["POST"])
+@authentication_classes(AuthenticationMiddleware)
+@permission_classes(IsAuthenticatedUser)
 def create_comment(request, event_id, *args, **kwargs):
     event = get_object_or_404(Events, pk=event_id)
     current_time = datetime.utcnow()
