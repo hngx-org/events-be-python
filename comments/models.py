fc48d9aa631fb26c7986f1ba6bd5dca0a9d6a724
Ifechukwu Ogidi <ogidiifechukwu6@gmail.com>
2023-10-08 12:09:22 +0100

    Feat: Created the Comments model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,17 @@
+from uuid import uuid4
+
 from django.db import models
 
+
+from events.models import Events
+from users.models import CustomUser
+
+
 # Create your models here.
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    comment = models.CharField(max_length=255)
+    created_at = models.DateField()
+    updated_at = models.DateField()
+    event_id = models.ForeignKey(Events, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
