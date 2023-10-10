70fac81caf00bc7b20572314514b8a1bb93a8164
timmyades3 <oluwatimileyinadesina623@gmail.com>
2023-10-10 13:07:37 +0100

    updated views.py

--- events/views.py
+++ events/views.py
@@ -51,24 +51,11 @@ class getEvent(APIView):
             HTTP_404_NOT_FOUND)
             
 
-class UpdateEventView(UpdateAPIView):
-    queryset = Events.objects.all()  
-    serializer_class = EventsSerializer  
-    lookup_url_kwarg = 'event_id'  
-
-    def get(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
-
-    def put(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class EventUpdate(generics.RetrieveUpdateAPIView):
+    # permission_classes=[IsAuthenticated]
+    queryset= Events.objects.all()
+    serializer_class=EventsSerializer
+    lookup_field='id'
 
 class SearchEventView(APIView):
     """
