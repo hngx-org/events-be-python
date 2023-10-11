963f42f7bf3a15fb966bfc23ea46fe81f479f53c
timmyades3 <oluwatimileyinadesina623@gmail.com>
2023-10-12 00:29:00 +0100

    test deployment

--- core/settings.py
+++ core/settings.py
@@ -181,7 +181,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
