7e8fdb631a6863b926c2a995b88214cb1fa36833
Konlan21 <joekonlan16@gmail.com>
2023-10-11 17:00:12 +0000

    Update requirements.txt

--- core/settings.py
+++ core/settings.py
@@ -122,7 +122,7 @@ DATABASES = {
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
-    # 
+    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('PGDATABASE'),
