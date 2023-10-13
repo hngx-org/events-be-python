85af52bcbe54ce7b02139a4065aa27d2d7dedf70
Yuguda999 <95680262+Yuguda999@users.noreply.github.com>
2023-10-14 00:16:16 +0100

    Added support for storing voice notes in comments

--- comments/models.py
+++ comments/models.py
@@ -15,12 +15,11 @@ def generateUUID():
 # Create your models here.
 class Comment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    comment = models.CharField(max_length=255)
+    comment = models.CharField(max_length=255, null=True, blank=True)
     created_at = models.DateTimeField(default=datetime.utcnow)
     updated_at = models.DateTimeField(default=datetime.utcnow)
     event_id = models.ForeignKey(Events, on_delete=models.CASCADE)
-    created_by = models.ForeignKey(UserSocialAuth, on_delete=models.CASCADE,
-                                   default=None)
+    created_by = models.ForeignKey(UserSocialAuth, models.CASCADE, blank=True, null=True)
     picture = models.ImageField(upload_to='comment_pictures/', null=True, blank=True)
     voice_note = models.FileField(upload_to='comment_voice_notes/', null=True, blank=True)
 
--- comments/views.py
+++ comments/views.py
@@ -13,15 +13,17 @@ from events.models import Events
 from rest_framework.permissions import IsAuthenticated
 from social_django.models import UserSocialAuth
 from rest_framework.views import APIView
-from drf_yasg.utils import swagger_auto_schema  # Import swagger_auto_schema
+from drf_yasg.utils import swagger_auto_schema  
+from django.core.files.base import ContentFile  
 
 
 class CommentCreateView(APIView):
     permission_classes = [IsAuthenticated]
     parser_classes = [MultiPartParser, FormParser]
-    @swagger_auto_schema(  # Add this decorator to document the view
-        request_body=CommentSerializer,  # Specify the serializer for the request body
-        operation_description="Create a comment with an image and voice note.",
+
+    @swagger_auto_schema(
+        request_body=CommentSerializer,
+        operation_description="Create a comment with an image, voice note, or text comment.",
         responses={
             201: "Comment created successfully",
             400: "Bad Request - Invalid data",
@@ -29,26 +31,50 @@ class CommentCreateView(APIView):
             500: "Internal Server Error",
         },
     )
-
-    def post(self, request, event_id, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         try:
+            event_id = request.data.get('event_id')  # Get event_id from request.data
             event = get_object_or_404(Events, pk=event_id)
             current_time = datetime.utcnow()
 
-            request.data['created_at'] = current_time
-            request.data['updated_at'] = current_time
-            request.data['event_id'] = event.id
-            request.data['created_by'] = request.user.id
+            comment_data = {
+                "comment": request.data.get('comment'),
+                "event_id": event_id,
+                "created_by": request.user.id
+            }
+            
+            # Extract the voice note file and handle it separately
+            voice_note = request.data.get('voice_note')
+            picture = request.data.get('picture')
 
-            comment = CommentSerializer(data=request.data)
+            # Check if at least one of the fields is provided
+            if not any([comment_data['comment'], voice_note, picture]):
+                return Response({"error": "At least one of comment, voice note, or picture must be provided."}, status=status.HTTP_400_BAD_REQUEST)
+
+            data = comment_data.copy()  # Create a mutable copy of comment_data
+            data['created_at'] = current_time
+            data['updated_at'] = current_time
+            data['event'] = event.id
+
+            comment = CommentSerializer(data=data)
 
             if comment.is_valid():
                 comment.save()
-                data = {
-                    "comment": comment.data["comment"],
+                
+                comment_instance = comment.instance
+
+                if voice_note:
+                    # Use ContentFile to properly save binary data to FileField
+                    comment_instance.voice_note.save(f"{comment_instance.id}.mp3", ContentFile(voice_note.read()))
+                if picture:
+                    comment_instance.picture = picture
+                comment_instance.save()
+                
+                response_data = {
+                    "comment": comment_instance.comment,
                     "event_id": event_id,
                 }
-                return Response(data, status=status.HTTP_201_CREATED)
+                return Response(response_data, status=status.HTTP_201_CREATED)
             else:
                 return Response(comment.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -57,7 +83,7 @@ class CommentCreateView(APIView):
 
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
+        
 class CommentListAPIView(generics.ListAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
