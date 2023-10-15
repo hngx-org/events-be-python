6aadd2bbdbc2b96c1f7b7a8a3419053f21a1b4a6
Adeyemo Oluwadamilare Emmanuel <vinceoludare@gmail.com>
2023-10-15 11:35:46 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -73,7 +73,7 @@ ACTIVATE_JWT = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        # 'users.utils.CsrfExemptSessionAuthentication',
+        'users.utils.CsrfExemptSessionAuthentication',
 
         # drf-social-oauth2
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',  # django-oauth-toolkit >= 1.0.0
